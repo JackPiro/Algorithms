8d5f34492a40663d446b7be18b4ca6db28de6ec5
Jack Piro <jackpiro1133@gmail.com>
2023-05-26 23:20:19 -0700

    fronts core

---
+++ singly_linked_lists/fronts_core.js
@@ -0,0 +1,45 @@
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+
+    addFront(value) {
+        let newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    removeFront() {
+        if (this.head) {
+            this.head = this.head.next;
+        }
+    }
+
+    front() {
+        if(this.head) {
+            return this.head.data;
+        } else {
+            return null;
+        }
+    }
+}
+
+let sll = new SLL(); 
+
+sll.addFront(18); 
+console.log(sll.front()); 
+
+sll.addFront(5);
+console.log(sll.front());
+
+sll.removeFront()
+console.log(sll.front());
+
+
